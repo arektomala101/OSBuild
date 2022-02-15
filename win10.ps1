8758279def05e28fab3bc667f4150c4478bac63a
arektomala101 <96944080+arektomala101@users.noreply.github.com>
2022-02-15 15:32:50 +0100

    Update win10.ps1

--- win10.ps1
+++ win10.ps1
@@ -25,34 +25,6 @@ Start-OSDCloud -OSLanguage "en-us" -OSBuild "21H1" -OSEdition Enterprise -OSLice
 Write-Host  -ForegroundColor Cyan "DImage deployment completed. Starting OSDCloud PostAction ..."
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 
-##########################
-# Setting up xml file
-##########################
-$oobe = @"
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-  <settings pass="oobeSystem">
-    <component name="Microsoft-Windows-International-Core" processorArchitecture="wow64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <InputLocale>0409:00000409</InputLocale>
-      <SystemLocale>en-US</SystemLocale>
-      <UILanguage>en-US</UILanguage>
-      <UserLocale>en-US</UserLocale>
-    </component>
-  </settings>
-</unattend>
-"@
-
-#updating xml file
-$xmlFile = "C:\Windows\Panther\Invoke-OSDSpecialize.xml"
-[xml]$parent = Get-Content -Path $xmlFile
-$parentnode = $parent.unattend.settings
-
-$child = [xml] ($oobe)
-$childNode = $child.SelectSingleNode("/*/*/*")
-$importedNode = $parent.ImportNode($childNode,$true)
-$parentnode.InsertAfter($importedNode, $parentnode.LastChild)
-Write-Host("Modified XML to: $($parent.OuterXML)")
-$parent.Save($xmlFile)
 ########################
 
 
